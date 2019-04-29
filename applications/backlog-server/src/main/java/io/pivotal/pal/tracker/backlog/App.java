fc9a6ea12a445687bb42f3aaee82b770355f4c21
Your Name <you@example.com>
2019-04-29 14:37:19 -0700

    test

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/App.java
@@ -13,7 +13,7 @@ import java.util.TimeZone;
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.backlog", "io.pivotal.pal.tracker.restsupport"})
 public class App {
-
+//comment
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
